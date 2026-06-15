abf3bf207b5bc05a60f544d07483c68173cc31f5
RishiMaskare <maskarerishi54@gmail.com>
2026-06-15 19:11:12 +0530

    Update 03_JobSequencingProblem.cpp

--- Striver-SDE-Sheet-Challenge/Day-15/03_JobSequencingProblem.cpp
+++ Striver-SDE-Sheet-Challenge/Day-15/03_JobSequencingProblem.cpp
@@ -24,6 +24,33 @@ O(MaxDeadline)
 
 --------------------------------------------------------
 */
+class Solution {
+  public:
+    vector<int> jobSequencing(vector<int> &deadline, vector<int> &profit) {
+        // code here
+        vector<pair<int, int>> jobs;
+        int n = deadline.size(), md = -1, totProfit = 0, cnt = 0;
+        for(int i=0;i<n;i++) {
+            jobs.push_back({profit[i], deadline[i]});
+            md = max(md, deadline[i]);
+        }
+        sort(jobs.rbegin(), jobs.rend());
+        vector<int> hash(md + 1, -1);
+        for(int i=0;i<n;i++) {
+            int p = jobs[i].first;
+            int d = jobs[i].second;
+            for(int j=d;j>0;j--) {
+                if(hash[j] == -1) {
+                    cnt++;
+                    totProfit += p;
+                    hash[j] = 1;
+                    break;
+                }
+            }
+        }
+        return {cnt, totProfit};
+    }
+};
 
 
 class Solution {
